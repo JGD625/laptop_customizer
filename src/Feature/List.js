ac96f4807b169dfa6c90e80027592de0217f864c
Julie <39500350+JGD625@users.noreply.github.com>
2019-04-21 19:13:03 -0400

    Create List.js

---
+++ src/Feature/List.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './List.css';
+
+class List extends Componenet {
+    render(){
+return ( 
+    <div className="list-returned">
+ <section className="main__form">
+ <h3>TECH SPECS AND CUSTOMIZATIONS</h3>
+ { features }
+  </section>
+
+
+</div>
+
+)
+};
+}
+
+export default List;
