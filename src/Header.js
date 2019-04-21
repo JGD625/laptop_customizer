e391adb805ed404b6d69b1892dd7a3da5fd04c9c
Julie <39500350+JGD625@users.noreply.github.com>
2019-04-21 19:14:29 -0400

    Create Header.js

---
+++ src/Header.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './Header.css'
+
+class Header extends Component{
+
+    render() {
+        return(
+            <header>
+          <h1>ELF Computing</h1>
+          <h3>Laptops</h3>
+          <h5>Customize your laptop</h5>  
+        </header> 
+        )
+    };
+    }
+
+    export default Header; 
